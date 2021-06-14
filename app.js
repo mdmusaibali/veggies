02e0bd5a2c6face7f8aa18b1ca8716b5804b652e
Mohammed Musaib Ali <64194019+mdmusaibali@users.noreply.github.com>
2021-06-14 20:29:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
 var app = express();
-let port=process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
@@ -15,13 +14,12 @@ var connection = mysql.createConnection({
 });
 
 app.get("/", function (req, res) {
-//   // Find count of users in DB
-//   var q = "SELECT COUNT(*) AS count FROM users";
-//   connection.query(q, function (err, results) {
-//     if (err) throw err;
-//     var count = results[0].count;
-//     res.render("home", { count: count });
-  res.render("home");
+  // Find count of users in DB
+  var q = "SELECT COUNT(*) AS count FROM users";
+  connection.query(q, function (err, results) {
+    if (err) throw err;
+    var count = results[0].count;
+    res.render("home", { count: count });
   });
 });
 
@@ -35,6 +33,6 @@ app.post("/register", function (req, res) {
   });
 });
 
-app.listen(port, function () {
+app.listen(3000, function () {
   console.log("Server running on 3000!");
 });
